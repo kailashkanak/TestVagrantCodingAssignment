df845c319de90ead1df5483a1d56aa442bcdd717
Kailash Kanak <106726096+kailashkanak@users.noreply.github.com>
2023-10-30 15:34:06 +0530

    Update TestingCode.java

--- TestingCode.java
+++ TestingCode.java
@@ -3,7 +3,22 @@ package coding.assignment;
 public class TestingCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		RecentlyPlayedSongsStore store = new RecentlyPlayedSongsStore(3);
+
+        store.playSong("S1", "user1");
+        store.playSong("S2", "user1");
+        store.playSong("S3", "user1");
+
+        System.out.println(store.getRecentlyPlayedSongs("user1")); 
+
+        store.playSong("S4", "user1");
+        System.out.println(store.getRecentlyPlayedSongs("user1")); 
+
+        store.playSong("S2", "user1");
+        System.out.println(store.getRecentlyPlayedSongs("user1")); 
+
+        store.playSong("S1", "user1");
+        System.out.println(store.getRecentlyPlayedSongs("user1")); 
 
 	}
 
